bcd49d5385c68c2c20f0f7e137df78b356ef96c3
NguyenXuanTiep05 <NguyenPetr2005@gmail.com>
2026-08-05 11:03:44 +0200

    login callback

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,22 +2,25 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function proxy(request: NextRequest) {
-  if (process.env.NODE_ENV === "development") {
+    // if (process.env.NODE_ENV === "development") {
+    //     return NextResponse.next();
+    // }
+
+    const isLoggedIn = request.cookies.has("token");
+    const { pathname } = request.nextUrl;
+
+    if (!isLoggedIn && pathname !== "/login") {
+        const loginUrl = new URL("/login", request.url);
+        const response = NextResponse.redirect(loginUrl);
+        response.cookies.set("callbackUrl", pathname, { maxAge: 300 });
+        return response;
+    }
+    if (isLoggedIn && pathname === "/login") {
+        return NextResponse.redirect(new URL("/", request.url));
+    }
     return NextResponse.next();
-  }
-
-  const isLoggedIn = request.cookies.has("token");
-  const { pathname } = request.nextUrl;
-
-  if (!isLoggedIn && pathname !== "/login") {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
-  if (isLoggedIn && pathname === "/login") {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
-  return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico|.*\\.png$).*)"],
+    matcher: ["/((?!_next/static|_next/image|favicon.ico|.*\\.png$).*)"],
 };
