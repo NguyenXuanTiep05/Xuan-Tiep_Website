b596837be681011c3d16661e028a0926b9803f8f
NguyenXuanTiep05 <nguyenpetr2005@gmail.com>
2026-08-14 20:33:40 +0700

    ResumeView, /dashboard

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,29 +2,33 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function proxy(request: NextRequest) {
-    if (process.env.NODE_ENV === "development") {
-        return NextResponse.next();
-    }
+	if (process.env.NODE_ENV === "development") {
+		return NextResponse.next();
+	}
 
-    const isLoggedIn = request.cookies.has("token");
-    const { pathname } = request.nextUrl;
+	const isLoggedIn = request.cookies.has("token");
+	const { pathname } = request.nextUrl;
 
-    if (!isLoggedIn && pathname !== "/login") {
-        const loginUrl = new URL("/login", request.url);
-        const response = NextResponse.redirect(loginUrl);
-        response.cookies.set("callbackUrl", pathname, {
-            maxAge: 300,
-            httpOnly: true,
-            secure: true,
-        });
-        return response;
-    }
-    if (isLoggedIn && pathname === "/login") {
-        return NextResponse.redirect(new URL("/", request.url));
-    }
-    return NextResponse.next();
+	if (pathname == "/") {
+		return NextResponse.next();
+	}
+
+	if (!isLoggedIn && pathname !== "/login") {
+		const loginUrl = new URL("/login", request.url);
+		const response = NextResponse.redirect(loginUrl);
+		response.cookies.set("callbackUrl", pathname, {
+			maxAge: 300,
+			httpOnly: true,
+			secure: true,
+		});
+		return response;
+	}
+	if (isLoggedIn && pathname === "/login") {
+		return NextResponse.redirect(new URL("/dashboard", request.url));
+	}
+	return NextResponse.next();
 }
 
 export const config = {
-    matcher: ["/((?!_next/static|_next/image|favicon.ico|.*\\.png$).*)"],
+	matcher: ["/((?!_next/static|_next/image|favicon.ico|.*\\.png$).*)"],
 };
